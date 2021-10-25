2ff0b07a66ce3bb9ebb535b792f761def6864801
ishimasar <ishimasar1106@gmail.com>
2021-10-25 15:40:13 +0900

    imageminWebpをいったん隠す

--- imagemin.js
+++ imagemin.js
@@ -1,21 +1,19 @@
 const imagemin = require('imagemin-keep-folder');
-const imageminWebp = require('imagemin-webp');
 const imageminMozjpeg = require('imagemin-mozjpeg');
 const imageminPngquant = require('imagemin-pngquant');
+// const imageminWebp = require('imagemin-webp');
 const imageminGifsicle = require('imagemin-gifsicle');
 const imageminSvgo = require('imagemin-svgo');
 
 imagemin(['src/img/*.{webp,jpg,png,gif,svg,ico}'], {
   plugins: [
-    imageminWebp({
-      quality: 80,
-    }),
     imageminMozjpeg({
       quality: 85,
     }),
     imageminPngquant({
       quality: 90,
     }),
+    // imageminWebp(),
     imageminGifsicle(),
     imageminSvgo(),
   ],
